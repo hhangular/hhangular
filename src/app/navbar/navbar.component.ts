1d670c5a66b7d6d7aee15adc79431a6a3679f448
hhfrancois-figaro <francois.achache@gmail.com>
2019-07-19 16:14:52 +0200

    fix url from package.json for repository access

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,12 +1,25 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {faGithub} from '@fortawesome/free-brands-svg-icons';
+import {GithubService} from '../core/github.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css'],
 })
-export class NavBarComponent {
+export class NavBarComponent implements OnInit {
 
   faGithub = faGithub;
+
+  url: string;
+
+  constructor(private githubService: GithubService) {
+  }
+
+  public ngOnInit(): void {
+    this.githubService.getPackageJson('').subscribe(json => {
+      this.url = json.repositry.url;
+    });
+  }
+
 }
