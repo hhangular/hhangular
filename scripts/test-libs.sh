657b110fa10d9b4468db9a1862e07cfa50c05c4f
hhfrancois-figaro <francois.achache@gmail.com>
2019-07-21 16:47:09 +0200

    disabled launch test e2e

--- scripts/test-libs.sh
+++ scripts/test-libs.sh
@@ -10,6 +10,6 @@ do
   echo Test lib : ${dir}
   echo ===========
   npm run test ${dir} -- --no-watch --no-progress --browsers=ChromeHeadless
-  npm run e2e ${dir} -- --protractor-config=e2e/protractor-ci.conf.js
+#  npm run e2e ${dir} -- --protractor-config=e2e/protractor-ci.conf.js
   codecov -f coverage/${dir}/* -t $CODECOV_TOKEN
 done
