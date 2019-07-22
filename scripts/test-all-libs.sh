b7ff9542cf17cbfc3fa6eaeff6a43907ffc0f319
hhfrancois-figaro <francois.achache@gmail.com>
2019-07-22 13:44:49 +0200

    add coverage in script...

--- scripts/test-all-libs.sh
+++ scripts/test-all-libs.sh
@@ -9,7 +9,7 @@ do
   dir="${dir##*/}"
   echo Test lib : ${dir}
   echo ===========
-  npm run test ${dir} -- --no-watch --no-progress --browsers=ChromeHeadless
+  npm run test ${dir} -- --no-watch --no-progress --code-coverage --browsers=ChromeHeadless
 #  npm run e2e ${dir} -- --protractor-config=e2e/protractor-ci.conf.js
   codecov -f coverage/${dir}/lcov.info --disable=gcov -t $CODECOV_TOKEN
 done
--- scripts/test-lib.sh
+++ scripts/test-lib.sh
@@ -4,7 +4,7 @@ echo ===========================================================================
 echo Test lib $1
 echo ==================================================================================================
 if [[ -d "projects/$1" ]]; then
-  npm run test $1 -- --no-watch --no-progress --browsers=ChromeHeadless
+  npm run test $1 -- --no-watch --no-progress --code-coverage --browsers=ChromeHeadless
 #  npm run e2e $1 -- --protractor-config=e2e/protractor-ci.conf.js
   ls -lta
   ls -lta coverage
