9230d66a73c25637360405cbe33b87c23d01a88e
hhfrancois-figaro <francois.achache@gmail.com>
2019-09-18 13:13:14 +0200

    generate sitemaps from json

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,7 @@ FROM node:10 as builder
 WORKDIR /tmp
 
 COPY . /tmp
-RUN npm install @angular/cli && npm install && npm run build:ssr && npm run copy:static
+RUN npm install @angular/cli && npm install && npm run build:ssr && npm run copy:static && npm run build:sitemap
 
 FROM node:10
 
--- static.copy.js
+++ static.copy.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-const resources = ['sitemap.xml', 'sitemap-en-us.xml', 'sitemap-fr-fr.xml', 'googlea7f29c4e268b216e.html'];
+const resources = ['googlea7f29c4e268b216e.html'];
 for(let resource of resources) {
   fs.copyFile(`resources/${resource}`, `dist/website/${resource}`, (err) => {
     if (err) throw err;
